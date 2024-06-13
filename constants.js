2d6519e51a926818a51de1463315427db1b5e314
erwannlenoach <erwannlenoach@hotmail.fr>
2024-06-13 16:40:57 +0200

    feat/functional-model-prediction

---
+++ constants.js
@@ -0,0 +1,42 @@
+const industry_sectors = {
+    'Utilities': 6,
+    'Healthcare': 5,
+    'Telecommunications': 4,
+    'Consumer Goods': 3,
+    'Technology': 3,
+    'Manufacturing': 3,
+    'Finance': 2,
+    'Retail': 2,
+    'Agriculture': 2,
+    'Transportation': 2,
+    'Real Estate': 1,
+    'Energy': 1,
+  };
+  
+  const regions = {
+    'Northern America': 6,
+    'Northern Europe': 6,
+    'Oceania': 5,
+    'Western Europe': 4,
+    'Southern Europe': 4,
+    'East Asia': 5,
+    'South-East Asia': 3,
+    'South Asia': 3,
+    'Central Asia': 3,
+    'Eastern Europe': 3,
+    'North Africa': 3,
+    'Southern Africa': 3,
+    'South America': 3,
+    'Middle East': 3,
+    'Central America': 2,
+    'East Africa': 2,
+    'West Africa': 2,
+  };
+
+  credit_ratings = ['Aaa', 'Aa1', 'Aa2', 'Aa3', 'A1', 'A2', 'A3',
+                  'Baa1', 'Baa2', 'Baa3', 'Ba1', 'Ba2', 'Ba3',
+                  'B1', 'B2', 'B3', 'Caa1', 'Caa2', 'Caa3', 'Ca', 'C']
+
+
+
+  module.exports = { industry_sectors, regions, credit_ratings };
