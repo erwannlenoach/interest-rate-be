7f7070037395150eff4a5b15ea0ed515c25abed1
erwannlenoach <erwannlenoach@hotmail.fr>
2024-07-09 09:15:21 +0200

    feat/update-routes

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,10 @@
-const express = require("express");
-const {
-  connexionUser,
-  createUsers,
-  getUser,
-  editPassword,
-} = require("../controllers/users");
+const express = require('express');
+const { createUsers, connexionUser, authenticateToken, getUser, editPassword } = require('../controllers/users');
 const router = express.Router();
 
-router.post("/users/profile", getUser);
-router.post("/users/connexion", connexionUser);
-router.post("/users/register", createUsers);
-router.post("/users/password/edit", editPassword);
+router.post('/register', createUsers);
+router.post('/login', connexionUser);
+router.get('/profile', authenticateToken, getUser);
+router.post('/password/edit', editPassword);
 
 module.exports = router;
